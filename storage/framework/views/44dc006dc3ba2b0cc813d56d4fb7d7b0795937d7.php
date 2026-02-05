0ac93b5109293852b4861b65c87ad99b925e3d90
Antonio Jose <aj@updev.net.br>
2026-02-04 21:48:47 -0300

    Remoção dos arquivos desnecessários

--- storage/framework/views/44dc006dc3ba2b0cc813d56d4fb7d7b0795937d7.php
+++
@@ -1,2 +0,0 @@
-<a  class="list-group-item p-l-r-0 d-flex"><?php echo e(auth()->user()->name ?? "***"); ?></a>
-<a  class="list-group-item p-l-r-0 d-flex"><?php echo e(auth()->user()->email ?? "***"); ?></a>
